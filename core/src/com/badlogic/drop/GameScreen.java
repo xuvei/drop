c23c04ce8550993cc5d0e870bc2ff9be5f39a049
Sandra Lemańska <lemasank@gmail.com>
2023-03-31 18:21:31 +0200

    1st commit

---
+++ core/src/com/badlogic/drop/GameScreen.java
@@ -0,0 +1,155 @@
+package com.badlogic.drop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
+
+import java.util.Iterator;
+
+public class GameScreen implements Screen {
+    final Drop game;
+
+    Texture dropImage;
+    Texture bucketImage;
+    Sound dropSound;
+    Music rainMusic;
+    OrthographicCamera camera;
+    Rectangle bucket;
+    Array<Rectangle> raindrops;
+    long lastDropTime;
+    int dropsGathered;
+
+    public GameScreen(final Drop game) {
+        this.game = game;
+
+        dropImage = new Texture(Gdx.files.internal("drop.png"));
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
+        dropSound = Gdx.audio.newSound(Gdx.files.internal("waterDropSound.wav"));
+        rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rainSounds.mp3"));
+        rainMusic.setLooping(true);
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 400);
+
+        bucket = new Rectangle();
+        bucket.x = 800/2 - 64/2;
+        bucket.y = 20;
+        bucket.width = 64;
+        bucket.height = 64;
+
+        raindrops = new Array<>();
+        spawnRaindrop();
+    }
+
+    private void spawnRaindrop() {
+        Rectangle raindrop = new Rectangle();
+        raindrop.x = MathUtils.random(0, 800-64);
+        raindrop.y = 400;
+        raindrop.width = 64;
+        raindrop.height = 64;
+        raindrops.add(raindrop);
+        lastDropTime = TimeUtils.nanoTime();
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0,0,0.2f,1);
+        camera.update();
+
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Drops collected: " + dropsGathered, 0, 480);
+        game.batch.draw(bucketImage, bucket.x, bucket.y, bucket.width, bucket.height);
+        for (Rectangle raindrop: raindrops) {
+            game.batch.draw(dropImage, raindrop.x, raindrop.y);
+        }
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            bucket.x = touchPos.x - 64/2;
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            bucket.x += 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        if (bucket.x < 0) {
+            bucket.x = 0;
+        }
+
+        if (bucket.x > 800 - 64) {
+            bucket.x = 800 - 64;
+        }
+
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) {
+            spawnRaindrop();
+        }
+
+        Iterator<Rectangle> iter = raindrops.iterator();
+        while (iter.hasNext()) {
+            Rectangle raindrop = iter.next();
+            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+            if (raindrop.y + 64 < 0) {
+                iter.remove();
+            }
+            if (raindrop.overlaps(bucket)) {
+                dropsGathered++;
+                dropSound.play();
+                iter.remove();
+            }
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+        rainMusic.play();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        dropImage.dispose();
+        bucketImage.dispose();
+        dropSound.dispose();
+        rainMusic.dispose();
+    }
+}
